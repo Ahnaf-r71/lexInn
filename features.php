d940310cde98499c02c972918dea176fa14319a7
Ahnaf-r71 <ahnafr71@gmail.com>
2024-09-12 17:03:11 +0600

    features updated

--- features.php
+++ features.php
@@ -1,8 +1,90 @@
 <?php include("reUse/header.php")  ?>
 
+<br><br><br>
 
+<style>
+    .pop:hover {
+    border-top-color: cyan !important;
+    transform: scale(1.1);
+    transition: 0.3s;
+}
+</style>
+  <!-- facilities -->
+  <h2 class="mt-5 pt-4 mb-4 text-center fw-bold h-font">Our Facilities</h2>
+  
+  <div class="container">
+    <div class="row">
+        <div class="col-lg-4 col-md-6 mb-5 px-4">
+            <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+                <div class="d-flex align-items-center mb-2">
+                    <img src="" width="40px">
+                    <h5 class="m-0 ms-3">
+                        wifi
+                    </h5>
+                </div>
+                <p>lelelelele</p>
+            </div>
+        </div>
+        <div class="col-lg-4 col-md-6 mb-5 px-4">
+            <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+                <div class="d-flex align-items-center mb-2">
+                    <img src="" width="40px">
+                    <h5 class="m-0 ms-3">
+                        wifi
+                    </h5>
+                </div>
+                <p>lelelelele</p>
+            </div>
+        </div>
+        <div class="col-lg-4 col-md-6 mb-5 px-4">
+            <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+                <div class="d-flex align-items-center mb-2">
+                    <img src="" width="40px">
+                    <h5 class="m-0 ms-3">
+                        wifi
+                    </h5>
+                </div>
+                <p>lelelelele</p>
+            </div>
+        </div>
+        <div class="col-lg-4 col-md-6 mb-5 px-4">
+            <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+                <div class="d-flex align-items-center mb-2">
+                    <img src="" width="40px">
+                    <h5 class="m-0 ms-3">
+                        wifi
+                    </h5>
+                </div>
+                <p>lelelelele</p>
+            </div>
+        </div>
+        <div class="col-lg-4 col-md-6 mb-5 px-4">
+            <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+                <div class="d-flex align-items-center mb-2">
+                    <img src="" width="40px">
+                    <h5 class="m-0 ms-3">
+                        wifi
+                    </h5>
+                </div>
+                <p>lelelelele</p>
+            </div>
+        </div>
+        <div class="col-lg-4 col-md-6 mb-5 px-4">
+            <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+                <div class="d-flex align-items-center mb-2">
+                    <img src="" width="40px">
+                    <h5 class="m-0 ms-3">
+                        wifi
+                    </h5>
+                </div>
+                <p>lelelelele</p>
+            </div>
+        </div>
+    </div>
+    
+  </div>
 
-
+17:16
 
 
 <?php include("reUse/footer.php")  ?>
